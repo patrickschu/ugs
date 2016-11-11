2bfb9c55c2879587fbb1a15505536a5eafa72024
patrick <patrickschultz@utexas.edu>
2016-11-11 09:47:10 -0600

    add psd output

--- matched.R
+++ matched.R
@@ -1,4 +1,4 @@
-matched=read.csv("/Users/ps22344/Documents/matchedguise.csv")
+matched=read.csv("/Users/ps22344/Downloads/ugs/matchedguise.csv")
 
 print (summary(matched))
 
@@ -13,16 +13,25 @@ plotter = function (dataset)
 	for (s in dataset[['STIMULUS']])
 	{
 		cat ("working on", s, "\n");
-		#print (dataset[dataset['STIMULUS'] == s,]);
-		
+		#print (summary(dataset[dataset['STIMULUS'] == s,]));
+		#print (levels(dataset['STIMULUS' == s,][['QUESTION']]));
 		for (q in levels(dataset['STIMULUS' == s,][['QUESTION']]))
-		{
-			#for each  question plot the distribution over options
-			qset=dataset[which(dataset['STIMULUS'] == s & dataset['QUESTION']== q),];
-			#print (qset);
-			counts=str(qset[['OPTION1']]);
-			print(counts)
-		}
+		 	{
+			#print (q)
+			# #for each  question plot the distribution over options
+			qset= dataset[which(dataset['STIMULUS'] == s & dataset['QUESTION']== q),];
+			options= c("Option 1"=qset[['OPTION1']], "Option 2"=qset[['OPTION2']], "Option 3"=qset[['OPTION3']]);
+			print (options);
+			barplot(options, 
+			ylim= c(0,15), 
+			col= c("white", "grey", "black"),
+			#density= c(100, 100, 100),
+			#col=c("red", "blue", "green"), 
+			main= paste("Stimulus", s)
+			)
+			# #counts=str(qset[['OPTION1']]);
+			# #print(counts)
+			}
 	}
 	
 	
