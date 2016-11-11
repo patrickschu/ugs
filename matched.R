ebc840e0ba2958839b9bafd9b5debcd84a8a2ee5
patrick <patrickschultz@utexas.edu>
2016-11-11 10:22:11 -0600

    improve title. final version

--- matched.R
+++ matched.R
@@ -1,5 +1,5 @@
 matched=read.csv("/Users/ps22344/Downloads/ugs/matchedguise.csv")
-
+setwd("/Users/ps22344/Downloads/ugs/rplots")
 print (summary(matched))
 
 
@@ -19,16 +19,18 @@ plotter = function (dataset)
 		 	{
 			#print (q)
 			# #for each  question plot the distribution over options
+			outputfile=paste("stimulus", s, q, sep="_")
+			postscript(paste(outputfile, ".ps"), width = 960, height = 960);
+			png(paste(outputfile, ".png"),  width=640, height=640, res=100);
 			qset= dataset[which(dataset['STIMULUS'] == s & dataset['QUESTION']== q),];
 			options= c("Option 1"=qset[['OPTION1']], "Option 2"=qset[['OPTION2']], "Option 3"=qset[['OPTION3']]);
 			print (options);
 			barplot(options, 
 			ylim= c(0,15), 
 			col= c("white", "grey", "black"),
-			#density= c(100, 100, 100),
-			#col=c("red", "blue", "green"), 
-			main= paste("Stimulus", s)
+			main= paste("Stimulus", s, ", ", sum (options), "respondents")
 			)
+			dev.off()
 			# #counts=str(qset[['OPTION1']]);
 			# #print(counts)
 			}
